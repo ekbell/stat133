d247b04f1f7230a31b6436a486563d84f8ffec41
Evan Bell <ekbell@berkeley.edu>
2015-03-12 10:39:52 -0700

    HW5/Project submission

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -8,9 +8,11 @@
 ## that stores the state of the system (i.e. location of red and blue cars)
 
 bml.init <- function(r, c, p){
-
-  
-   #return(m)
+  probs = c(1-p, p/2, p/2)
+  x <- c(0, 1, 2)
+  numbers <- sample(x, size = r*c, replace = TRUE, prob = probs)
+  m <- matrix(data = numbers, nrow = r, ncol = c)
+   return(m)
 }
 
 #### Function to move the system one step (east and north)
@@ -21,16 +23,107 @@ bml.init <- function(r, c, p){
 ## NOTE : the function should move the red cars once and the blue cars once,
 ## you can write extra functions that do just a step north or just a step east.
 
+blocked_red <- function(m){
+  new_m <- matrix(0, nrow = nrow(m), ncol = ncol(m))
+  for(i in 1:nrow(m)){
+    for(j in 1:(ncol(m)-1)){
+      if(m[i,j] == 1){
+        if(m[i,j+1]== 0){
+          new_m[i, j] = FALSE}
+        else{new_m[i,j] = TRUE}
+      }
+      if(m[i,j] != 1){new_m[i,j] = FALSE}
+    }
+    if(m[i,ncol(m)] == 1){
+      if(m[i,1]== 0){
+        new_m[i, ncol(m)] = FALSE}
+      else{new_m[i,ncol(m)] = TRUE}
+    }
+    if(m[i,j] != 1){new_m[i,j] = FALSE}
+  }
+  return(new_m)
+}
+
+
+blocked_blue <- function(m){
+  new_m <- matrix(0, nrow = nrow(m), ncol = ncol(m))
+  for(j in 1:ncol(m)){
+    if(m[1,j] == 2){
+      if(m[nrow(m),j]== 0){new_m[1, j] = FALSE}
+      if(m[nrow(m),j] != 0){new_m[1, j] = TRUE}
+    }
+    if(m[1,j] != 2){new_m[1, j] = FALSE}
+  }
+  for(i in 2:nrow(m)){
+    for(j in 1:ncol(m)){
+      if(m[i,j] != 2){new_m[i,j] = FALSE}
+      if(m[i,j] == 2){
+        if(m[i-1,j]== 0){new_m[i, j] = FALSE}
+        if(m[i-1,j] != 0){new_m[i,j] = TRUE}
+      }
+      if(m[i,j] != 2){new_m[i,j] = FALSE}
+    }}
+  return(new_m)
+}
+
+red_cars <- function(m){
+  new_m <- matrix(, nrow = nrow(m), ncol = ncol(m))
+  for(i in 1:nrow(m)){
+    for(j in 1:ncol(m)){
+      if(m[i,j] == 1){new_m[i,j] =1}
+      if(m[i,j] != 1){new_m[i,j] =0}
+    }}
+  return(new_m)
+}
+
+blue_cars <- function(m){
+  return(2*(m==2))
+}
+
+shift_right <- function(mat){
+  mat = mat[,c(ncol(mat), 1:ncol(mat)-1)]
+  return(mat)}
+
+shift_up <- function(mat){
+  mat = mat[c(2:(nrow(mat)),1),]
+  return(mat)}
+
+
 bml.step <- function(m){
+original_m <- m
+red_blocked <- red_cars(m)*blocked_red(m)
+red_not_blocked <- red_cars(m)*!blocked_red(m)
+m <- blue_cars(m) + shift_right(red_not_blocked) + red_blocked
+
+blue_blocked <- blue_cars(m)*blocked_blue(m)
+blue_not_blocked <- blue_cars(m)*!blocked_blue(m)
+m <- shift_up(blue_not_blocked) + (m == 1) + blue_blocked
+grid.new <- "TRUE"
+if(all(m == original_m)){grid.new = "FALSE"}
+   return(list(m, grid.new))
+}
+
 
   
-   #return(list(m, grid.new))
-}
 
 #### Function to do a simulation for a given set of input parameters
 ## Input : size of grid [r and c] and density [p]
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
 bml.sim <- function(r, c, p){
-
+  m <- bml.init(r, c, p)
+  max_it <- 1000
+  new_list <- bml.step(m)
+  for(i in 2:max_it){
+    new_list <- bml.step(new_list[[1]])
+    if(new_list[[2]] == FALSE){
+       return(i)
+    }
+  }
+  return(max_it)
 }
+
+
+
+
+
--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -7,4 +7,76 @@
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
 
+#Below are lists which record gridlock for three matrices: 10x10, 6x6, and 4x4.
+#Each matrix is run 1000 times for values of p at 0.25, 0.3, 0.5, 0.75, and 0.9. 
+
+
+#There are three sample matrices illustrated, one for each size. 
+matrix10 <- matrix(sample(0:2, size=100, replace=T), ncol=10)
+image(matrix10, col=c("white", "red", "blue"))
+image(t(matrix10), col=c("white", "red", "blue"))
+help(pdf)
+pdf(file='/Users/evanbell/src/stat133/hw5/matrix10_plot.pdf')
+image(matrix10, col=c("white", "red", "blue"))
+image(t(matrix10), col=c("white", "red", "blue"))
+dev.off()
+
+matrix6 <- matrix(sample(0:2, size=36, replace=T), ncol=6)
+image(matrix6, col=c("white", "red", "blue"))
+image(t(matrix6), col=c("white", "red", "blue"))
+help(pdf)
+pdf(file='/Users/evanbell/Documents/matrix6_plot.pdf')
+image(matrix6, col=c("white", "red", "blue"))
+image(t(matrix6), col=c("white", "red", "blue"))
+dev.off()
+
+matrix4 <- matrix(sample(0:2, size=16, replace=T), ncol=4)
+image(matrix4, col=c("white", "red", "blue"))
+image(t(matrix4), col=c("white", "red", "blue"))
+help(pdf)
+pdf(file='/Users/evanbell/Documents/matrix4_plot.pdf')
+image(matrix4, col=c("white", "red", "blue"))
+image(t(matrix4), col=c("white", "red", "blue"))
+dev.off()
+
+
+sim10a <- replicate(1000, bml.sim(10,10,0.25))
+sim10b <- replicate(1000, bml.sim(10,10,0.5))
+sim10c <- replicate(1000, bml.sim(10,10,0.75))
+sim10d <- replicate(1000, bml.sim(10,10,0.9))
+sim10e <- replicate(1000, bml.sim(10,10,0.3))
+
+
+sim6a <- replicate(1000, bml.sim(6,6,0.25))
+sim6b <- replicate(1000, bml.sim(6,6,0.5))
+sim6c <- replicate(1000, bml.sim(6,6,0.75))
+sim6d <- replicate(1000, bml.sim(6,6,0.9))
+sim6e <- replicate(1000, bml.sim(6,6,0.3))
+
+
+sim4a <- replicate(1000, bml.sim(4,4,0.25))
+sim4b <- replicate(1000, bml.sim(4,4,0.5))
+sim4c <- replicate(1000, bml.sim(4,4,0.75))
+sim4d <- replicate(1000, bml.sim(4,4,0.9))
+sim4e <- replicate(1000, bml.sim(4, 4, 0.3))
+
+
+
+boxplot(sim10a, sim10e, sim10b, sim10c, sim10d, names = c("0.25","0.3", "0.5", "0.75", "0.9"), xlab = "Value of P",
+        ylab= "Number of Moves Until Gridlock", main = "10x10 Matrix Simulation")
+
+boxplot(sim10c, sim10d, names = c("0.75", "0.9"), xlab = "Value of P",
+        ylab= "Number of Moves Until Gridlock", main = "10x10 Matrix Simulation", ylim = c(0,175))
+
+boxplot(sim6a, sim6e, sim6b, sim6c, sim6d, names = c("0.25","0.3", "0.5", "0.75", "0.9"), xlab = "Value of P",
+        ylab= "Number of Moves Until Gridlock", main = "6x6 Matrix Simulation")
+
+boxplot(sim6c, sim6d, names = c("0.75", "0.9"), xlab = "Value of P",
+        ylab= "Number of Moves Until Gridlock", main = "6x6 Matrix Simulation", ylim = c(0,150))
+
+boxplot(sim4a, sim4e, sim4b, sim4c, sim4d, names = c("0.25", "0.3", "0.5", "0.75", "0.9"), xlab = "Value of P",
+        ylab= "Number of Moves Until Gridlock", main = "4x4 Matrix Simulation")
+
+boxplot(sim4d, names = c("0.9"), xlab = "Value of P = 0.9",
+        ylab= "Number of Moves Until Gridlock", main = "4x4 Matrix Simulation", ylime = c(0, 30))
 
